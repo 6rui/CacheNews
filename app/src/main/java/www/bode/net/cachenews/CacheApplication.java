1295a31c1fe0bc4ee6dfe043bbbdb7eb393ffd9d
LrCache <411589769@qq.com>
2016-06-22 11:02:59 +0800

    Initial commit

---
+++ app/src/main/java/www/bode/net/cachenews/CacheApplication.java
@@ -0,0 +1,13 @@
+package www.bode.net.cachenews;
+
+import android.app.Application;
+
+/**
+ * 全局application Created by Liu on 2016-06-22.
+ */
+public class CacheApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
